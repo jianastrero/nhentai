c8f92a77522f779d04ed285400bd4aa18cce223a
Jian James Astrero <jianjamesastrero@gmail.com>
2021-04-10 01:05:21 +0800

    remove tags

--- common/src/commonMain/kotlin/com/jianastrero/common/model/Manga.kt
+++ common/src/commonMain/kotlin/com/jianastrero/common/model/Manga.kt
@@ -6,8 +6,7 @@ import com.jianastrero.common.ui.HOME_URL
 data class Manga(
     val id: String,
     val galleryId: String,
-    val title: String,
-    val tags: Array<String>
+    val title: String
 ) {
 
     fun url() = "${HOME_URL}g/$id"
@@ -15,26 +14,4 @@ data class Manga(
     fun galleryUrl() = "$GALLERY_URL$id/"
 
     fun thumbnailUrl() = "${galleryUrl()}thumb.jpg"
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Manga
-
-        if (id != other.id) return false
-        if (galleryId != other.galleryId) return false
-        if (title != other.title) return false
-        if (!tags.contentEquals(other.tags)) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = id.hashCode()
-        result = 31 * result + galleryId.hashCode()
-        result = 31 * result + title.hashCode()
-        result = 31 * result + tags.contentHashCode()
-        return result
-    }
 }
