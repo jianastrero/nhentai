f0a05dc61261471c07cbc6c13409ce65f768e40f
Jian James Astrero <jianjamesastrero@gmail.com>
2021-04-09 23:53:51 +0800

    manga url getters

--- common/src/commonMain/kotlin/com/jianastrero/common/model/Manga.kt
+++ common/src/commonMain/kotlin/com/jianastrero/common/model/Manga.kt
@@ -1,11 +1,21 @@
 package com.jianastrero.common.model
 
+import com.jianastrero.common.ui.GALLERY_URL
+import com.jianastrero.common.ui.HOME_URL
+
 data class Manga(
     val id: String,
     val galleryId: String,
     val title: String,
     val tags: Array<String>
 ) {
+
+    fun url() = "${HOME_URL}g/$id"
+
+    fun galleryUrl() = "$GALLERY_URL$id/"
+
+    fun thumbnailUrl() = "${galleryUrl()}thumb.jpg"
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
