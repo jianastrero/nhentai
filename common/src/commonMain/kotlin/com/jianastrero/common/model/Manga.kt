2ed629274339e7069557b7536a699687a83dbdc3
Jian James Astrero <jianjamesastrero@gmail.com>
2021-04-10 12:23:13 +0800

    add thumbnailExtension property

--- common/src/commonMain/kotlin/com/jianastrero/common/model/Manga.kt
+++ common/src/commonMain/kotlin/com/jianastrero/common/model/Manga.kt
@@ -6,12 +6,13 @@ import com.jianastrero.common.ui.HOME_URL
 data class Manga(
     val id: String,
     val galleryId: String,
-    val title: String
+    val title: String,
+    val thumbnailExtension: String
 ) {
 
     fun url() = "${HOME_URL}g/$id"
 
     fun galleryUrl() = "$GALLERY_URL$galleryId/"
 
-    fun thumbnailUrl() = "${galleryUrl()}thumb.jpg"
+    fun thumbnailUrl() = "${galleryUrl()}thumb.$thumbnailExtension"
 }
