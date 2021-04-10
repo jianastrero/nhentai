7f55721427eb0182568fbe33fbbda2ff2f9a08c5
Jian James Astrero <jianjamesastrero@gmail.com>
2021-04-10 12:50:10 +0800

    put Home on a seperate file and implement showing of popular manga

---
+++ common/src/commonMain/kotlin/com/jianastrero/common/ui/Home.kt
@@ -0,0 +1,48 @@
+package com.jianastrero.common.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.jianastrero.common.enumeration.AppStatus
+import com.jianastrero.common.model.Manga
+import com.jianastrero.common.view.Header
+import com.jianastrero.common.view.MangaItem
+import com.jianastrero.common.viewmodel.HomeViewModel
+import org.jsoup.nodes.Document
+
+internal val homeViewModel = HomeViewModel()
+
+@Composable
+fun Home(block: (AppStatus, Document?) -> Unit) {
+
+    homeViewModel.fetch(block)
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        PopularManga(homeViewModel.popularMangas)
+    }
+}
+
+@Composable
+fun PopularManga(popularMangas: List<Manga>) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Header("Popular Now")
+        LazyRow(
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+            items(popularMangas) { MangaItem(it) }
+        }
+    }
+}
