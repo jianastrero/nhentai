b650eabb1f5c4630db2475e68fa09f0614324e5d
Jian James Astrero <jianjamesastrero@gmail.com>
2021-04-10 21:35:59 +0800

    Move StaggeredVerticalGrid.kt to its own file

--- common/src/commonMain/kotlin/com/jianastrero/common/ui/Home.kt
+++ common/src/commonMain/kotlin/com/jianastrero/common/ui/Home.kt
@@ -8,8 +8,6 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.Layout
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.jianastrero.common.enumeration.AppStatus
 import com.jianastrero.common.enumeration.Platform
@@ -17,9 +15,9 @@ import com.jianastrero.common.model.Manga
 import com.jianastrero.common.platform.getPlatform
 import com.jianastrero.common.view.Header
 import com.jianastrero.common.view.MangaItem
+import com.jianastrero.common.view.StaggeredVerticalGrid
 import com.jianastrero.common.viewmodel.HomeViewModel
 import org.jsoup.nodes.Document
-import kotlin.math.ceil
 
 internal val homeViewModel = HomeViewModel()
 
@@ -76,59 +74,4 @@ fun AllManga(allManga: List<Manga>) {
             allManga.forEach { MangaItem(it, true, 0) }
         }
     }
-}
-
-@Composable
-fun StaggeredVerticalGrid(
-    modifier: Modifier = Modifier,
-    maxColumnWidth: Dp,
-    content: @Composable () -> Unit
-) {
-    Layout(
-        content = content,
-        modifier = modifier
-    ) { measurables, constraints ->
-        check(constraints.hasBoundedWidth) {
-            "Unbounded width not supported"
-        }
-        val columns = ceil(constraints.maxWidth / maxColumnWidth.toPx()).toInt()
-        val columnWidth = constraints.maxWidth / columns
-        val itemConstraints = constraints.copy(maxWidth = columnWidth)
-        val colHeights = IntArray(columns) { 0 } // track each column's height
-        val placeables = measurables.map { measurable ->
-            val column = shortestColumn(colHeights)
-            val placeable = measurable.measure(itemConstraints)
-            colHeights[column] += placeable.height
-            placeable
-        }
-
-        val height = colHeights.maxOrNull()?.coerceIn(constraints.minHeight, constraints.maxHeight)
-            ?: constraints.minHeight
-        layout(
-            width = constraints.maxWidth,
-            height = height
-        ) {
-            val colY = IntArray(columns) { 0 }
-            placeables.forEach { placeable ->
-                val column = shortestColumn(colY)
-                placeable.place(
-                    x = columnWidth * column,
-                    y = colY[column]
-                )
-                colY[column] += placeable.height
-            }
-        }
-    }
-}
-
-private fun shortestColumn(colHeights: IntArray): Int {
-    var minHeight = Int.MAX_VALUE
-    var column = 0
-    colHeights.forEachIndexed { index, height ->
-        if (height < minHeight) {
-            minHeight = height
-            column = index
-        }
-    }
-    return column
 }
