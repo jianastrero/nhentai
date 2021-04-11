7b15393ad5bdaaa50990d532cca14d0e59f38c28
Jian James Astrero <jianjamesastrero@gmail.com>
2021-04-11 18:26:54 +0800

    fetch necessary data from manga

--- common/src/commonMain/kotlin/com/jianastrero/common/viewmodel/ViewMangaDetailsViewModel.kt
+++ common/src/commonMain/kotlin/com/jianastrero/common/viewmodel/ViewMangaDetailsViewModel.kt
@@ -2,7 +2,11 @@ package com.jianastrero.common.viewmodel
 
 import androidx.compose.runtime.*
 import com.jianastrero.common.controller.SERVER_UPDATED_ERROR_CONTROLLER
+import com.jianastrero.common.extension.getFirstElementByClass
+import com.jianastrero.common.extension.getFirstElementByTag
 import com.jianastrero.common.model.Manga
+import com.jianastrero.common.model.Tag
+import com.jianastrero.common.model.TagList
 import com.jianastrero.common.statemachine.StateMachine
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -12,6 +16,10 @@ class ViewMangaDetailsViewModel {
 
     lateinit var manga: Manga
 
+    val tags = mutableListOf<TagList>()
+
+    var pages = 0
+
     @Composable
     fun fetch() {
         if (!::manga.isInitialized) {
@@ -20,24 +28,58 @@ class ViewMangaDetailsViewModel {
 
         var state by remember { mutableStateOf(-1) }
 
-        GlobalScope.launch {
-            try {
-                val document = Jsoup.connect(manga.url()).get()
-
-                println(document.html())
-                state = 1
-            } catch (e: Exception) {
-                e.printStackTrace()
-                state = 0
+        when (state) {
+            0 -> {
+                StateMachine.finish()
+                StateMachine.start(SERVER_UPDATED_ERROR_CONTROLLER)
             }
-            manga.url()
-        }
+            1 -> StateMachine.nextState()
+            else -> {
+                tags.clear()
+                pages = 0
 
-        if (state == 0) {
-            StateMachine.finish()
-            StateMachine.start(SERVER_UPDATED_ERROR_CONTROLLER)
-        } else if (state == 1) {
-            StateMachine.nextState()
+                GlobalScope.launch {
+                    try {
+                        val document = Jsoup.connect(manga.url()).get()
+
+                    println(document.html())
+
+                        val _tags = document
+                            .getElementById("tags")
+                            .getElementsByClass("tag-container")
+
+                        _tags.forEach {
+                            var title: String
+                            val list = mutableListOf<Tag>()
+                            it.also {
+                                title = it.html().subSequence(0, it.html().indexOf(':')).trim().toString()
+                                println("title: $title")
+                            }.getFirstElementByClass("tags")
+                                .getElementsByTag("a")
+                                .forEach {
+                                    it.getFirstElementByTag("a")?.let { aTag ->
+                                        val urlSlug = aTag.attr("href")
+                                        val _title = aTag.getFirstElementByClass("name").html()
+
+                                        if (title == "Pages") {
+                                            pages = _title.toInt()
+                                        } else {
+                                            val count = aTag.getFirstElementByClass("count").html()
+
+                                            list.add(Tag(_title, urlSlug, count))
+                                        }
+                                    }
+                                }
+                            tags.add(TagList(title, list))
+                        }
+
+                        state = 1
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                        state = 0
+                    }
+                }
+            }
         }
     }
 
