8d6512c19f93fbfc984356720f4b38a306669979
Jian James Astrero <jianjamesastrero@gmail.com>
2021-04-11 14:06:24 +0800

    add instance of ViewMangaDetailsViewModel on ViewMangaDetails.kt

--- common/src/commonMain/kotlin/com/jianastrero/common/ui/ViewMangaDetails.kt
+++ common/src/commonMain/kotlin/com/jianastrero/common/ui/ViewMangaDetails.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import com.jianastrero.common.statemachine.StateMachine
+import com.jianastrero.common.viewmodel.ViewMangaDetailsViewModel
 
+internal val viewMangaDetailsViewModel = ViewMangaDetailsViewModel()
 
 @Composable
 fun ViewMangaDetails() {
