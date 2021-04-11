63a50a9076ff9bd8231110f489795d9c70d13dfa
Jian James Astrero <jianjamesastrero@gmail.com>
2021-04-11 16:26:44 +0800

    simplify HomeViewModel.kt

--- common/src/commonMain/kotlin/com/jianastrero/common/viewmodel/HomeViewModel.kt
+++ common/src/commonMain/kotlin/com/jianastrero/common/viewmodel/HomeViewModel.kt
@@ -21,60 +21,62 @@ class HomeViewModel {
     fun fetch() {
         var state by remember { mutableStateOf(-1) }
 
-        GlobalScope.launch {
-            try {
-                val document = Jsoup.connect(HOME_URL).get()
-
-                val popularNow = document
-                    .select(".index-container")
-                    .select(".index-popular")
-                    .first()
-
-                popularNow.getElementsByClass("gallery").forEach {
-                    val id = it.getFirstElementByTag("a").attr("href").split("/")[2]
-                    val img = it.getFirstElementByTag("img")
-                        .attr("data-src")
-                        .replace("http://", "")
-                        .replace("https://", "")
-                        .split("/")
-                    val galleryId = img[2]
-                    val thumbnailExtension = img[3].split('.')[1]
-                    val title = it.getFirstElementByClass("caption").html()
-
-                    val manga = Manga(id, galleryId, title, thumbnailExtension)
-                    MangaMapDatabase.insert(manga)
-                    popularMangas.add(manga)
-                }
-
-                popularNow.remove()
-
-                document.getElementsByClass("gallery").forEach {
-                    val id = it.getFirstElementByTag("a").attr("href").split("/")[2]
-                    val img = it.getFirstElementByTag("img")
-                        .attr("data-src")
-                        .replace("http://", "")
-                        .replace("https://", "")
-                        .split("/")
-                    val galleryId = img[2]
-                    val thumbnailExtension = img[3].split('.')[1]
-                    val title = it.getFirstElementByClass("caption").html()
-
-                    val manga = Manga(id, galleryId, title, thumbnailExtension)
-                    MangaMapDatabase.insert(manga)
-                    allManga.add(manga)
-                }
-                state = 1
-            } catch (e: Exception) {
-                e.printStackTrace()
-                state = 0
+        when (state) {
+            0 -> {
+                StateMachine.finish()
+                StateMachine.start(SERVER_UPDATED_ERROR_CONTROLLER)
             }
-        }
+            1 -> StateMachine.nextState()
+            else -> {
+                GlobalScope.launch {
+                    try {
+                        val document = Jsoup.connect(HOME_URL).get()
 
-        if (state == 0) {
-            StateMachine.finish()
-            StateMachine.start(SERVER_UPDATED_ERROR_CONTROLLER)
-        } else if (state == 1) {
-            StateMachine.nextState()
+                        val popularNow = document
+                            .select(".index-container")
+                            .select(".index-popular")
+                            .first()
+
+                        popularNow.getElementsByClass("gallery").forEach {
+                            val id = it.getFirstElementByTag("a").attr("href").split("/")[2]
+                            val img = it.getFirstElementByTag("img")
+                                .attr("data-src")
+                                .replace("http://", "")
+                                .replace("https://", "")
+                                .split("/")
+                            val galleryId = img[2]
+                            val thumbnailExtension = img[3].split('.')[1]
+                            val title = it.getFirstElementByClass("caption").html()
+
+                            val manga = Manga(id, galleryId, title, thumbnailExtension)
+                            MangaMapDatabase.insert(manga)
+                            popularMangas.add(manga)
+                        }
+
+                        popularNow.remove()
+
+                        document.getElementsByClass("gallery").forEach {
+                            val id = it.getFirstElementByTag("a").attr("href").split("/")[2]
+                            val img = it.getFirstElementByTag("img")
+                                .attr("data-src")
+                                .replace("http://", "")
+                                .replace("https://", "")
+                                .split("/")
+                            val galleryId = img[2]
+                            val thumbnailExtension = img[3].split('.')[1]
+                            val title = it.getFirstElementByClass("caption").html()
+
+                            val manga = Manga(id, galleryId, title, thumbnailExtension)
+                            MangaMapDatabase.insert(manga)
+                            allManga.add(manga)
+                        }
+                        state = 1
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                        state = 0
+                    }
+                }
+            }
         }
     }
 }
