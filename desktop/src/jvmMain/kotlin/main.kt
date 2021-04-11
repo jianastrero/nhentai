1f9811608471f4a12199b66f1cd01202b09f1597
Jian James Astrero <jianjamesastrero@gmail.com>
2021-04-11 23:50:35 +0800

    set window name

--- desktop/src/jvmMain/kotlin/main.kt
+++ desktop/src/jvmMain/kotlin/main.kt
@@ -1,10 +1,11 @@
 
 import androidx.compose.desktop.LocalAppWindow
 import androidx.compose.desktop.Window
+import com.jianastrero.common.platform.getAppName
 import com.jianastrero.common.ui.App
 
 fun main(args: Array<String>) {
-    Window {
+    Window(getAppName()) {
         val window = LocalAppWindow.current
         window.maximize()
 
